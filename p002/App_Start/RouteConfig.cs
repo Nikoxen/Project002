199bf4870d4e498b0b6ac1a3914375b68a8205c8
Nadide YILMAZ <29244280+Nikoxen@users.noreply.github.com>
2021-07-28 20:19:56 +0200

    son
    son ...

--- p002/App_Start/RouteConfig.cs
+++ p002/App_Start/RouteConfig.cs
@@ -12,10 +12,16 @@ namespace p002
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+
+              routes.MapRoute(
+              name: "Home",
+              url: "Home/{action}",
+              defaults: new { controller = "Home", action = "Index" }
+           );
+               routes.MapRoute(
+               name: "Default",
+               url: "{controller}/{action}/{id}",
+               defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
